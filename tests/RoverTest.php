7828ae94ffa66a6f005311470c17c4601edf7952
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-11-01 14:59:28 +0100

    refactor tests with a dataprovider

--- tests/RoverTest.php
+++ tests/RoverTest.php
@@ -3,32 +3,30 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Iterator;
 use PhpKataSetup\Rover;
 use PHPUnit\Framework\TestCase;
 
 class RoverTest extends TestCase
 {
+    static function movingForwardProvider(): Iterator  {
+        yield 'moving once' => ['0:1:N', 'M'];
+        yield 'moving twice' => ['0:2:N', 'MM'];
+        yield 'moving third' => ['0:3:N', 'MMM'];
+    }
+
+    /**
+     * @test
+     * @dataProvider movingForwardProvider
+     */
+    function given_a_command_forward_when_rover_is_in_landing_position(string $position, string $command) {
+        $rover = new Rover();
+        self::assertSame($position, $rover->move($command));
+    }
+
     /** @test */
     function given_landing_position_returns_0_0_N() {
         $rover = new Rover();
         self::assertSame('0:0:N', $rover->move(''));
-    }
-
-    /** @test */
-    function given_a_command_forward_when_rover_is_in_landing_position_returns_0_1_N() {
-        $rover = new Rover();
-        self::assertSame('0:1:N', $rover->move('M'));
-    }
-
-    /** @test */
-    function given_two_command_forward_when_rover_is_in_landing_position_returns_0_2_N() {
-        $rover = new Rover();
-        self::assertSame('0:2:N', $rover->move('MM'));
-    }
-
-    /** @test */
-    function given_three_command_forward_when_rover_is_in_landing_position_returns_0_3_N() {
-        $rover = new Rover();
-        self::assertSame('0:3:N', $rover->move('MMM'));
     }
 }
